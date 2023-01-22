d71dcf93495214e594b04cc4360a7af83e0cbddb
Kirill K <websiteproduct@gmail.com>
2023-01-22 23:56:58 +0100

    finalize

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const toggleBtn = document.querySelector('#menu-toggler')
 const menu = document.querySelector('.main-menu')
 
+
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('show')
 })
+
+function handleIntersection(entries) {
+    entries.map((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible', entry.target.dataset.anim)
+        }
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersection)
+const animatedElements = document.querySelectorAll('.animate__animated')
+
+animatedElements.forEach(el => observer.observe(el))
