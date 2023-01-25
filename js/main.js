e7a6a5cc11aebf944d95c6574bc8d0e228821e40
Kirill K <websiteproduct@gmail.com>
2023-01-25 01:49:53 +0100

    some changes

--- js/main.js
+++ js/main.js
@@ -24,3 +24,28 @@ const observer = new IntersectionObserver(handleIntersection)
 const animatedElements = document.querySelectorAll('.animate__animated')
 
 animatedElements.forEach(el => observer.observe(el))
+
+const nextBtn = document.querySelector('#next-review')
+const reviewsContainer = document.querySelector('.reviews-container')
+const reviewCounter = document.querySelectorAll('.review-item').length
+const dotsContainer = document.querySelector('.dots')
+let activePosition = 1
+
+for (let i = 0; i < reviewCounter; i++) {
+    const dot = document.createElement('div')
+    dot.classList.add('dot')
+    dot.dataset.slide = i
+    if (i === 0) dot.classList.add('active')
+    dotsContainer.append(dot)
+}
+
+const dots = document.querySelectorAll('.dot')
+
+nextBtn.addEventListener('click', () => {
+    dots[activePosition - 1].classList.remove('active')
+    if (activePosition > reviewCounter) activePosition = 1
+    
+    reviewsContainer.style.transform = `translateX(${activePosition * -33.33333333}%)`
+    activePosition++
+    dots[activePosition - 1].classList.add('active')
+})
